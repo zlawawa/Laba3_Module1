969ca02926cfb9d6449e1a4be14202e402b4f20a
Злава <zlatakyhalova@gmail.com>
2025-11-13 19:33:45 +0500

    Update Pr_main.cpp

--- Pr_main.cpp
+++ Pr_main.cpp
@@ -11,41 +11,46 @@ int main(){
     #endif
 
     int Task;
-    std::cout << "Выберите номер задачи 1-5: \n";
-    std::cout << "1. Задача №1 \n";
-    std::cout << "2. Задача №2 \n";
-    std::cout << "3. Задача №3 \n";
-    std::cout << "4. Задача №4 \n";
-    std::cout << "5. Задача №5 \n";
-    std::cout << "6. Задача №6 \n";
-    std::cin >> Task;
-    switch (Task){
-        case 1: {
-            output1();
-            break;
+    std::cout << "Выберите пункт 1-6: \n";
+    std::cout << "1. Задача №1 Proc28 \n";
+    std::cout << "2. Задача №2 Proc12 \n";
+    std::cout << "3. Задача №3 Proc45 \n";
+    std::cout << "4. Задача №4 TheTri18 \n";
+    std::cout << "5. Задача №5 Reccur11 \n";
+    std::cout << "6. Выход из программы \n";
+    while (Task != 6){
+        std::cin >> Task;
+        switch (Task){
+            case 1: {
+                taskProc28();
+                break;
+                }
+            case 2: {
+                taskProc12();
+                break;
             }
-        case 2: {
-            output2();
-            break;
-        }
-        case 3: {
-            output3();
-            break;
-        }
-        case 4: {
-            output4();
-            break;
-        }
-        case 5: {
-            output5();
-            break;
-        }
-        default: {
-            std::cout << "error\n";
-            break;
+            case 3: {
+                taskProc45();
+                break;
+            }
+            case 4: {
+                taskTheTri18();
+                break;
+            }
+            case 5: {
+                taskReccur11();
+                break;
+            }
+            case 6: {
+                std::cout << "Вы вышли из программы.\n";
+                return 0;
+                break;
+            }
+            default:
+                std::cout << "Введены неверные данные\n";
+                break;
         }
     }
-    return 0;
 }
 
 
